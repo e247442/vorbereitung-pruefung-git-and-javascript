66f869b36b44ef145fd0516b147734efc43bffc2
e247442 <e247442@edu.sbl.ch>
2022-12-13 17:03:07 +0100

    acceleration

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 const circle = document.querySelector("#circ")
 
+let dy = 0
+
 
 function start() {
     window.requestAnimationFrame(update)
 }
 
 function update() {
+    dy = dy + 0.05
     console.log("Hello World")
 
     let cy = parseFloat(circle.getAttribute("cy"))
-    cy = cy + 1
+    cy = cy + dy
     circle.setAttribute("cy", cy)
 
     window.requestAnimationFrame(update)
